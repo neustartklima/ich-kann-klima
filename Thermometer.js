3c1a5f898fba024c11c4bf16ed05aa6a36fe850b
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-03-23 22:19:34 +0100

    Added thermometer

---
+++ Thermometer.js
@@ -0,0 +1,19 @@
+export default function Thermometer(el, questions) {
+  const getSum = (array, func) => array.reduce((sum, current) => sum + func(current), 0)
+  const getMax = (array, func) => array.reduce((max, current) => Math.max(max, func(current)), -1e38)
+  const getMin = (array, func) => array.reduce((min, current) => Math.min(min, func(current)), 1e38)
+  
+  const max = getSum(questions, (question) => getMax(question.answers, (answer) => answer.co2))
+  const min = getSum(questions, (question) => getMin(question.answers, (answer) => answer.co2))
+
+  let factor = 10 / (max - min)
+  let value = 10 + min * factor
+
+  console.log({factor, value, min, max})
+  el.setAttribute("height", value)
+
+  return {
+    addCO2Reduction: (offset) => el.setAttribute("height", value += +offset * factor),
+    getValue: () => value * 10
+  }
+}
