666ab7964d753589e1c8905ec27497e02b07b63d
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-04-24 05:27:08 +0200

    Fixed paths to old frontend

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-import Thermometer from "./frontend/components/Thermometer.js"
-import Slider from "./frontend/components/Slider.js"
-import CO2Value from "./frontend/CO2Value.js"
+import Thermometer from "./old-frontend/components/Thermometer.js"
+import Slider from "./old-frontend/components/Slider.js"
+import CO2Value from "./old-frontend/CO2Value.js"
 
 const elems = Object.assign({}, ...(["question", "infos", "slider", "result", "prev", "next", "restart", "thermometer"]
   .map((id) => ({[id]: document.querySelector("#" + id)}))))
