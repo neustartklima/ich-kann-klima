94f67e62c14a844c9c542521f9c903e2f155bf54
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-01 07:12:22 +0200

    Cleaned up gh_pages branch

--- frontend/src/store/state.ts
+++
@@ -1,9 +0,0 @@
-import { Game } from "../types"
-import { allLaws } from "../allLaws"
-
-export const state = {
-  allLaws,
-  game: undefined as Game | undefined,
-}
-
-export type State = typeof state
