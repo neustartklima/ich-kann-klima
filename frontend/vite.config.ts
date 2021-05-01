c9ef5ae519d6dd67ba9aa9b9c4f69ece0178609b
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-01 08:00:04 +0200

    Separated steps into separate jobs Fixed issues with 404 page

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { resolve } from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        "404": resolve(__dirname, "404.html"),
+      },
+    },
+  },
 })
