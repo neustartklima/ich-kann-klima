d24866ad5d95ca5f2d4352c5a0651c7803d04821
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-19 06:47:11 +0200

    Use only frontend in top-level

--- server/Model.spec.ts
+++
@@ -1,9 +0,0 @@
-import "mocha"
-import "should"
-import { initialValues } from "./Model"
-
-describe("Model", () => {
-  it("should return initial values", () => {
-    initialValues().should.have.properties(["co2Emmissions", "stateDebt", "unemployment", "gdp"])
-  })
-})
