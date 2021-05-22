c124e3ec865e750dc9a7c51653d9c013e0d471a7
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-22 07:32:40 +0200

    Fix: load laws in built mode

--- src/Factory.ts
+++ src/Factory.ts
@@ -2,7 +2,7 @@ import { Event, Law, LawDefinition } from "./types"
 
 export function createLaw(url: string, law: LawDefinition): Law {
   const id = url
-    .replace(/\.ts(\?.*)?$/, "")
+    .replace(/\.[tj]s(\?.*)?$/, "")
     .split(/[\\/]/)
     .pop() as string
   return { ...law, id }
