94b101a5e79b438596455b046b40cc6897c44c36
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-29 18:53:54 +0200

    Initiate events every now and then

--- src/EventMachine.ts
+++ src/EventMachine.ts
@@ -1,10 +1,37 @@
 import { Store } from "./store"
 import { Event } from "./types"
 
-export function getAnEvent(store: Store, allEvents: Event[]): Event | null {
-  const priorizedEvents = allEvents
-    .map((event) => ({ ...event, priority: event.probability(store) * Math.random() }))
-    .filter((event) => event.priority)
-    .sort((a, b) => b.priority - a.priority)
-  return  priorizedEvents.length ? priorizedEvents[0] : null
+export default function(store: Store, allEvents: Event[]) {
+  let timer: number
+
+  function getPriorizedEvents(): Event[] {
+    return allEvents
+      .map((event) => ({ ...event, priority: event.probability(store) * Math.random() }))
+      .filter((event) => event.priority)
+      .sort((a, b) => b.priority - a.priority)
+  }
+
+  function initiateEvent() {
+    const game = store.state.game
+    if (game) {
+      const events = getPriorizedEvents()
+      if (events.length) {
+        store.dispatch("applyEvent", { event: events[0] })
+      }
+    }
+  }
+
+  return {
+    start() {
+      // Check for events once in a minute
+      timer = setTimeout(initiateEvent, Math.random() * 60000)
+    },
+
+    pause() {
+      timer && clearTimeout(timer)
+      timer = 0
+    },
+
+    initiateEvent,
+  }
 }
--- tests/EventMachine.spec.ts
+++ tests/EventMachine.spec.ts
@@ -1,8 +1,8 @@
 import should from "should"
-import { getAnEvent } from "../src/EventMachine"
+import EventMachine from "../src/EventMachine"
 import { Event, Game } from "../src/types"
 import { createBaseValues, defaultValues } from "../src/repository"
-import { store } from "../src/store"
+import { Store } from "../src/store"
 
 const game: Game = {
   id: "1",
@@ -22,33 +22,49 @@ function createEvent(title: string, probability: number): Event {
   }
 }
 
-describe("EventMachine.getOne()", () => {
-  it("should return the most probable event", () => {
+const dispatched = []
+
+const store = {
+  state: { game },
+  dispatch(type: string, data: unknown) {
+    dispatched.push({ type, data })
+  },
+} as Store
+
+function countEvents(allEvents: Event[]): Record<string, number> {
+  const eventMachine = EventMachine(store, allEvents)
+  dispatched.length = 0
+  const agg = Object.assign({}, ...allEvents.map(event => ({ [event.title]: 0 })))
+  ;[...Array(100).keys()].forEach(() => eventMachine.initiateEvent())
+  dispatched
+    .filter((event) => event.type === "applyEvent")
+    .forEach((event) => agg[event.data.event.title]++)
+  return agg
+}
+
+describe("EventMachine.initiateEvent()", () => {
+  it("should return the most probable event most of the time", () => {
     const allEvents = [createEvent("t1", 0.1), createEvent("t2", 0.9)]
-    const found = [...Array(100).keys()].filter(() => getAnEvent(store, allEvents).title === "t1")
-    found.length.should.be.lessThan(10)
+    const stats = countEvents(allEvents)
+    stats.t1.should.be.lessThan(15)
   })
 
-  it("should use randomly choose from equally probable events", () => {
-    // check that both events are found in a reasonable time
+  it("should randomly choose from equally probable events", () => {
     const allEvents = [createEvent("t1", 0.8), createEvent("t2", 0.8)]
-    let current: string
-    let prev: string
-    do {
-      prev = current
-      current = getAnEvent(store, allEvents).title
-    } while (prev && current === prev)
+    const stats = countEvents(allEvents)
+    Math.abs(stats.t1 - stats.t2).should.be.lessThan(15)
   })
 
   it("should ignore impossible events", () => {
     // cannot be said for sure, but at least check that not 1 in 100 times this event occurs
     const allEvents = [createEvent("t1", 0), createEvent("t2", 0.5)]
-    const found = [...Array(100).keys()].some(() => getAnEvent(store, allEvents).title === "t1")
-    found.should.be.false()
+    const stats = countEvents(allEvents)
+    stats.t1.should.be.equal(0)
   })
 
   it("should return null if no event can be found", () => {
     const allEvents = [createEvent("t1", 0), createEvent("t2", 0)]
-    should(getAnEvent(store, allEvents)).be.null()
+    const stats = countEvents(allEvents)
+    should(stats).be.deepEqual({ t1: 0, t2: 0 })
   })
 })
