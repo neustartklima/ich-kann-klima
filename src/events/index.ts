8d2a31176f8f794111ecc165598e33e1289e7523
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-05-30 10:35:45 +0200

    Added new events to event list

--- src/events/index.ts
+++ src/events/index.ts
@@ -2,10 +2,14 @@ import bestechung from "./bestechung"
 import socialMedia from "./social-media"
 import wahlVerloren from "./WahlVerloren"
 import finanzKollaps from "./Finanzkollaps"
+import TimesUp from "./TimesUp"
+import Hitzehölle from "./Hitzehölle"
 
 export const allEvents = [
   socialMedia,
   bestechung,
   wahlVerloren,
   finanzKollaps,
+  TimesUp,
+  Hitzehölle,
 ]
