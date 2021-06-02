987bbabbcdcab1dba47338d82d6643ad8cf58408
Mathias de Riese <mdrie@fastmail.com>
2021-06-02 23:12:57 +0200

    Laws in Peek; sortable; effects visible on mouse hover.

--- src/components/PeekInside.vue
+++ src/components/PeekInside.vue
@@ -1,8 +1,9 @@
 <script lang="ts">
 import { computed, defineComponent } from "vue"
-import { store, useStore } from "../store"
-import { BaseParams, GameId } from "../types"
+import { useStore } from "../store"
+import { BaseParams, Game, GameId } from "../types"
 import { mapGetters } from "vuex"
+import { startYear } from "../constants"
 
 const sortOrder: BaseParams = {
   co2budget: 100,
@@ -29,29 +30,93 @@ const sortOrder: BaseParams = {
   gdp: 800,
 }
 
+function genCompare(a: number | string, b: number | string) {
+  if (a < b) return -1;
+  if (a > b) return 1;
+  return 0;
+}
+
+type LawSortCols = "state" | "id" | "priority"
+
 export default defineComponent({
   setup() {
     const store = useStore()
 
     return {
       store,
-      values: computed(() => store.state.game?.values),
-      allLaws: computed(() => store.state.allLaws),
-      acceptedLaws: computed(() => store.state.game?.acceptedLaws),
+      game: computed(() => store.state.game),
+    }
+  },
+
+  data() {
+    return {
+      lawsSortCol: "state" as LawSortCols,
+      lawsSortDir: 1,
+      lawSelected: undefined as string | undefined,
+    }
+  },
+  methods: {
+    sortLaws(column: LawSortCols) {
+      if (column === this.lawsSortCol) {
+        this.lawsSortDir = (this.lawsSortDir > 0) ? -1 : 1
+      }
+      this.lawsSortCol = column
+    },
+    selectLaw(id: string | undefined) {
+      this.lawSelected = id
     }
   },
 
   computed: {
-    sortedValues(): [string, number][] {
-      if (this.values === undefined)
-      {
+    effectsOfSelected(): Partial<BaseParams> {
+      if (!this.lawSelected || !this.game) return {}
+      const game: Game = this.game
+      const law = this.store.state.allLaws.find((law) => law.id === this.lawSelected)
+      if (!law) return {}
+      return law.effects(game.values, startYear, game.currentYear)
+    },
+
+    sortedValues(): [string, string, string][] {
+      if (this.game === undefined) {
         return []
       }
-      const vals: BaseParams = this.values
+      const values: BaseParams = this.game.values
+      const effects = this.effectsOfSelected
+      function format(effect: number | undefined)
+      {
+        if (effect) return effect.toFixed(2)
+        return "-"
+      }
       return Object.entries(sortOrder)
         .sort((a, b) => a[1] - b[1])
         .map(entry => entry[0])
-        .map(key => [key, vals[key as keyof BaseParams]])
+        .map(key => [key, values[key as keyof BaseParams].toFixed(2), format(effects[key as keyof BaseParams])])
+    },
+
+    sortedLaws(): {id: string, priority: string, state: string}[] {
+      if (this.game === undefined) {
+        return []
+      }
+      const game: Game = this.game
+      const sortCol: LawSortCols = this.lawsSortCol
+      const proposed = this.game.proposedLaws
+      const accepted = this.game.acceptedLaws.map(lawRef => lawRef.lawId)
+      const rejected = this.game.rejectedLaws
+      function findState(lawId: string)
+      {
+        if(accepted.includes(lawId)) return "a";
+        if(proposed.includes(lawId)) return "p";
+        if(rejected.includes(lawId)) return "r";
+        return "x"
+      }
+      return this.store.state.allLaws
+        .map(law => ({
+          id: law.id,
+          priority: law.priority(game),
+          state: findState(law.id),
+        }))
+        .sort((a, b) => genCompare(a[sortCol], b[sortCol]) * this.lawsSortDir)
+        .map(law => ({...law, priority: law.priority.toFixed(2)}))
     },
   },
 
@@ -62,9 +127,22 @@ export default defineComponent({
   <details class="peekData">
     <summary>Peek</summary>
     <table>
-      <tr v-for="[key, value] in sortedValues">
+      <tr v-for="[key, value, effect] in sortedValues" :key="key">
         <td>{{key}}</td>
-        <td>{{value}}</td>
+        <td class="numbercol">{{value}}</td>
+        <td>{{effect}}</td>
+      </tr>
+    </table>
+    <table>
+      <tr>
+        <th @click="sortLaws('state')">S</th>
+        <th @click="sortLaws('id')">ID</th>
+        <th @click="sortLaws('priority')" class="numbercol">Priority</th>
+      </tr>
+      <tr v-for="law in sortedLaws" :key="law.id" :class="law.state" @mouseenter="selectLaw(law.id)" @mouseleave="selectLaw(undefined)">
+        <td>{{law.state}}</td>
+        <td>{{law.id}}</td>
+        <td class="numbercol">{{law.priority}}</td>
       </tr>
     </table>
   </details>
@@ -75,6 +153,22 @@ export default defineComponent({
   padding: 0 5px;
   font-size: 12px;
   background: white;
+
+  .a {
+    background: lightblue
+  }
+
+  .p {
+    background: lightyellow
+  }
+
+  .r {
+    background: lightgrey
+  }
+
+  .numbercol {
+    text-align: right;
+  }
 
   &[open] {
     border: 1px solid #cccccc;
