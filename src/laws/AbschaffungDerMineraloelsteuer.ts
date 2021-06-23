128fe756363145949c864feaea4a013b238670b7
Mathias de Riese <mdrie@fastmail.com>
2021-06-23 21:28:47 +0200

    Correct use of carRenewablePercentage in 2 laws.

--- src/laws/AbschaffungDerMineraloelsteuer.ts
+++ src/laws/AbschaffungDerMineraloelsteuer.ts
@@ -28,7 +28,8 @@ export default defineLaw({
   priority(game) {
     const v = game.values
     const totalUsage = v.localTransportUsage + v.longdistanceTransportUsage + v.carUsage
-    const relCarPercentage: Percent = (v.carUsage / totalUsage) * 100
+    const carNonRenewableUsage = v.carUsage * (1 - v.carRenewablePercentage / 100)
+    const relCarPercentage: Percent = (carNonRenewableUsage / totalUsage) * 100
     return linear(70, 100, relCarPercentage)
   },
 })
