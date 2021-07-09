f71a7cc756173aaf27e4dc223c0db96683c70779
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-07-09 06:50:16 +0200

    Added missing tests, fixed some bugs

--- tests/FetchQueue.spec.ts
+++ tests/FetchQueue.spec.ts
@@ -28,13 +28,40 @@ describe("FetchQueue", () => {
       let callNo = 1
       const queue = FetchQueueFactory(() => (callNo++ < 2 ? Promise.reject(-1) : Promise.resolve(456)), { timeout: 1 })
       const result = (await queue.add("GET", "/abc", undefined, 2)) as number
-      result.should.equal(456)
       callNo.should.equal(3)
     })
 
-    it("should not repeat to call the fetch function when it succeeds")
+    it("should finally reject if the maximum number of retries is reached", async () => {
+      let callNo = 1
+      const queue = FetchQueueFactory(() => (callNo++ < 2 ? Promise.reject(-1) : Promise.resolve(456)), { timeout: 1 })
+      const result = (await queue.add("GET", "/abc", undefined, 2)) as number
+      result.should.equal(456)
+    })
 
-    it("should finally reject if the maximum number of retries is reached")
+    it("should keep the order of calls if one call fails at first", async () => {
+      let tryNo = 1
+      const callFunc = (callNo: string) =>
+        callNo === "GET" && tryNo++ === 1 ? Promise.reject(-1) : Promise.resolve(callNo)
+      const queue = FetchQueueFactory(callFunc, { timeout: 1 })
+      const results = await Promise.all([queue.add("GET", "/abc"), queue.add("POST", "/abc")])
+      results.should.deepEqual(["GET", "POST"])
+    })
+
+    it("should not repeat to call the fetch function when it succeeds", async () => {
+      const queue = FetchQueueFactory(() => Promise.resolve(42), { timeout: 1 })
+      await queue.add("GET", "/abc")
+      clock.next()
+      queue.callsPending().should.be.false()
+    })
+
+    it("should not repeat to call the fetch function when it finally fails", async () => {
+      const queue = FetchQueueFactory(() => Promise.reject(666), { timeout: 1 })
+      try {
+        await queue.add("GET", "/abc", undefined, 0)
+      } catch (error) {}
+      clock.next()
+      queue.callsPending().should.be.false()
+    })
   })
 
   describe("remove", () => {
@@ -52,6 +79,11 @@ describe("FetchQueue", () => {
       queue.remove("GET", "/abc")
     })
 
-    it("should ignore character case of method part when searching for entries")
+    it("should ignore character case of method part when searching for entries", async () => {
+      const queue = FetchQueueFactory(Sinon.stub().rejects(42))
+      queue.add("GET", "/abc", undefined, -1) as Promise<number>
+      queue.remove("get", "/abc")
+      queue.callsPending().should.be.false()
+    })
   })
 })
