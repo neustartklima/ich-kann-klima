36741ea5956e25ab21f991fea81f5f36261bc88f
Mathias de Riese <mdrie@fastmail.com>
2021-07-21 10:47:15 +0200

    paramDefinitions complete.

--- tests/Params.spec.ts
+++ tests/Params.spec.ts
@@ -1,5 +1,5 @@
-import "should"
-import { createInitialValues, paramDefinitions, ParamDefinitions, ParamKey } from "../src/model/Params"
+import should from "should"
+import { computedParamList, createBaseValues, defaultValues, ParamDefinitions, paramKeys } from "../src/model/Params"
 import { ParamDefinition } from "../src/types"
 
 describe("ParamDefinitions", () => {
@@ -9,24 +9,52 @@ describe("ParamDefinitions", () => {
   })
 })
 
-describe("createInitialValues()", () => {
+describe("createBaseValues(defaultValues)", () => {
+  const initVals = createBaseValues(defaultValues)
+
   it("should return a valid object of type Params", () => {
-    const params = createInitialValues()
-    for (const key in paramDefinitions) {
-      const name = key as ParamKey
-      params[name].should.be.of.type("number")
+    for (const key of paramKeys) {
+      initVals[key].should.be.of.type("number")
     }
   })
   it("should return an object with initial values set", () => {
-    const params = createInitialValues()
-    params.co2budget.should.be.equal(6700)
-    params.electricityCoal.should.be.equal(35.46 + 82.13)
+    initVals.co2budget.should.be.equal(6700)
+    initVals.electricityCoal.should.be.equal(35.46 + 82.13)
   })
   it("should return an object with computed values updated dynamically", () => {
-    const params = createInitialValues()
+    const params = createBaseValues(defaultValues)
     const sav = params.electricityCoal
     params.electricityBrownCoal += 100
     params.electricityCoal.should.be.equal(sav + 100)
   })
-  it("should return an object with computed values read-only")
+  it("should return an object with computed values read-only", () => {
+    for (const pd of computedParamList) {
+      should.throws(() => {
+        initVals[pd.name] = 999
+      })
+    }
+  })
+  it("should return an object with the computed values set approximately to shouldInitiallyBe", () => {
+    for (const pd of computedParamList) {
+      if (pd.shouldInitiallyBe) {
+        initVals[pd.name].should.approximately(
+          pd.shouldInitiallyBe,
+          pd.shouldInitiallyBe * 0.01,
+          "Computed initial value for " +
+            pd.name +
+            " should be " +
+            pd.shouldInitiallyBe +
+            " " +
+            pd.unit +
+            " but is " +
+            initVals[pd.name] +
+            " " +
+            pd.unit +
+            " according to " +
+            pd.sourcesDesc() +
+            "."
+        )
+      }
+    }
+  })
 })
