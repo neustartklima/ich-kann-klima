d5d01a9563b008b8f82df0d1eac57d6cc3d9e629
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-07-22 06:57:44 +0200

    Handle markdown

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,7 @@
+import {Converter} from "showdown"
+
+const converter = new Converter()
+
 function numSequence(amount: number): number[] {
   if (amount > 0) {
     return [...Array(amount)].map((_, i) => i)
@@ -9,4 +13,17 @@ function numSequence(amount: number): number[] {
 export function getZIndexes(numPlaces: number, centerPos: number): number[] {
   const descending = numPlaces - centerPos - 1
   return [...numSequence(centerPos), Math.max(centerPos, descending), ...numSequence(descending).reverse()]
+}
+
+export function markdown(text: TemplateStringsArray): string {
+  let strings = text[0].split("\n")
+  if (strings[0].length === 0) {
+    strings.shift()
+  }
+  const indent = strings[0].match(/^(\s+)/)
+  if (indent && !strings.filter(s => s.trim()).some(s => !s.startsWith(indent[1]))) {
+    strings = strings.map(s => s.replace(indent[1], ""))
+  }
+
+  return converter.makeHtml(strings.join("\n"))
 }
--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,4 +1,4 @@
-import { getZIndexes } from "../src/lib/utils"
+import { getZIndexes, markdown } from "../src/lib/utils"
 
 describe("utils", () => {
   describe("getZIndexes", () => {
@@ -13,5 +13,37 @@ describe("utils", () => {
     it("should return an descending sequence if centerPos equals 0", () => {
       getZIndexes(5, 0).should.deepEqual([4, 3, 2, 1, 0])
     })
+  })
+
+  describe("markdown", () => {
+    it("should convert a tagged template string with markdown to html", () => {
+      const string = markdown`
+# This is the title
+
+Some text
+
+- This is the first list item
+- This is the second list item
+      `
+
+      string.should.equal(
+        '<h1 id="thisisthetitle">This is the title</h1>\n' +
+          "<p>Some text</p>\n" +
+          "<ul>\n" +
+          "<li>This is the first list item</li>\n" +
+          "<li>This is the second list item</li>\n" +
+          "</ul>"
+      )
+    })
+
+    it("should work with indented markdown if all is indented the same", () => {
+      markdown`
+        # title
+
+        text
+      `.should.equal(`<h1 id="title">title</h1>\n<p>text</p>`)
+    })
+
+    it("should work with code snippets inside indented markdown")
   })
 })
