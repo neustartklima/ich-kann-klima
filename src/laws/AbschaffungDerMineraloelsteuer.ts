ceb6660d98093305c82794a3cf32e1e3be05f40b
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-07-22 06:57:44 +0200

    Handle markdown

--- src/laws/AbschaffungDerMineraloelsteuer.ts
+++ src/laws/AbschaffungDerMineraloelsteuer.ts
@@ -1,9 +1,9 @@
-import { urlencoded } from "express"
 import { defineLaw } from "../Factory"
 import { changeMioPsgrKmBy, changePercentBy, linear } from "../lawTools"
 import { welt2018BundKassiertMineraloelsteuer } from "../sources"
 import { MrdEuro, Percent } from "../types"
 import { WritableBaseParams } from "../params"
+import { markdown } from "../lib/utils"
 
 export default defineLaw({
   title: "Abschaffung der Mineralölsteuer",
@@ -36,16 +36,21 @@ export default defineLaw({
   },
   sources: [welt2018BundKassiertMineraloelsteuer],
   details: /*html*/ ``,
-  internals: /*html*/ `
-  <h1>Folgen</h1>
-    <p>Staatsschulden steigen um 41 Mrd € pro Jahr [welt2018BundKassiertMineraloelsteuer]</p>
-    <p>Im ersten Jahr steigen 20% der Nutzer von öffentlichen Verkehrsmitteln aufs Auto um.</p>
-    <p>Popularität steigt im ersten Jahr um 5% und sinkt danach um 3% pro Jahr.</p>
-  <h1>Vorbedingungen</h1>
-    <p>Priorität über 0%.</p>
-  <h1>Priorität:</h1>
-    <p>0% bei einem Anteil von nichterneuerbaren PKW von 60%.</p>
-    <p>100% bei einem Anteil von nichterneuerbaren PKW von 100%. (Zu Beginn: 78%)</p>
-    <p>linear interpoliert</p>
+  internals: markdown`
+# Folgen
+
+Staatsschulden steigen um 41 Mrd € pro Jahr [welt2018BundKassiertMineraloelsteuer]
+Im ersten Jahr steigen 20% der Nutzer von öffentlichen Verkehrsmitteln aufs Auto um.
+Popularität steigt im ersten Jahr um 5% und sinkt danach um 3% pro Jahr.
+
+# Vorbedingungen
+
+- Priorität über 0%.
+
+# Priorität
+
+- 0% bei einem Anteil von nichterneuerbaren PKW von 60%.
+- 100% bei einem Anteil von nichterneuerbaren PKW von 100%. (Zu Beginn: 78%)
+- linear interpoliert
     `,
 })
