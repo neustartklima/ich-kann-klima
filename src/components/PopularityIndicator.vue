dafb2d87b264b65eca44613d30e6460c303763e6
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-07-27 09:21:08 +0200

    Added popularity indicator with window images

---
+++ src/components/PopularityIndicator.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  props: {
+    value: { type: Number, required: true },
+  },
+
+  computed: {
+    areHappy(): boolean {
+      return this.value >= 50;
+    },
+
+    areAngry(): boolean {
+      return this.value < 50;
+    },
+  },
+})
+</script>
+
+<template>
+  <div id="poularity">
+    <img v-if="areHappy" src="../assets/people-happy.png" id="happy" />
+    <img v-if="areAngry" src="../assets/people-angry.png" id="angry" />
+  </div>
+</template>
+
+<style lang="scss">
+#poularity {
+  position: absolute;
+  left: 24.8%;
+  top: 58%;
+  width: 49.3%;
+
+  img {
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+  }
+}
+</style>
