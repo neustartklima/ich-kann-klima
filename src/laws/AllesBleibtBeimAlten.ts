fbd47ef1d1bf41798dcb7ee53d5b6ef4942ef46f
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-07-27 09:26:06 +0200

    Adapted some more laws

--- src/laws/AllesBleibtBeimAlten.ts
+++ src/laws/AllesBleibtBeimAlten.ts
@@ -1,12 +1,11 @@
 import { defineLaw } from "../Factory"
-import { WritableBaseParams } from "../params"
 
 export default defineLaw({
   title: "Alles bleibt wie es ist",
   description: "Die vorhandenen Gesetze haben sich lange bewährt. Wir lassen sie so, wie sind sind.",
 
-  effects(): Partial<WritableBaseParams> {
-    return {}
+  effects() {
+    return []
   },
 
   priority(game) {
