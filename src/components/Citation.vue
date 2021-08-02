10859762a5c00fadd74566c345e52e75fd8fb031
Mathias de Riese <mdrie@fastmail.com>
2021-08-02 09:45:50 +0200

    Show details of selected param.

--- src/components/Citation.vue
+++ src/components/Citation.vue
@@ -33,8 +33,8 @@ export default defineComponent({
     <span v-if="citation.authors">{{ citation.authors + date }}{{ sep }}</span>
     <span><a :href="href">"{{ title }}"</a>{{ sep }}</span>
     <span v-if="citation.publisher">{{ citation.publisher }}{{ sep }}</span>
-    <span v-if="citation.comment">Comment: {{ citation.comment }}{{ sep }}</span>
-    <span v-if="showInternals && citation.internalComment">Internal: {{ citation.internalComment }}{{ sep }}</span>
+    <span v-if="citation.comment">Bemerkung: <span v-html="citation.comment"/>{{ sep }}</span>
+    <span v-if="showInternals && citation.internalComment">Internes: <span v-html="citation.internalComment"/>{{ sep }}</span>
     <span v-if="citation.referringUrl"><a :href="citation.referringUrl.toString()">Found here</a>{{ sep }}</span>
     <span v-if="citation.archiveUrl"><a :href="citation.archiveUrl.toString()">Archive link</a>{{ sep }}</span>
     <span v-if="citation.localCopy"><a :href="'/assets/sources/' + citation.localCopy">Local copy</a>{{ sep }}</span>
