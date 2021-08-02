017bdcc0285b6ccd3f29b115140e522f3f583324
Mathias de Riese <mdrie@fastmail.com>
2021-08-02 19:07:32 +0200

    Peek for events with details with one example.

--- src/events/WindkraftAusschreibung.ts
+++ src/events/WindkraftAusschreibung.ts
@@ -3,6 +3,8 @@ import AusschreibungsverfahrenfuerWindkraftWieBisher from "../laws/Ausschreibung
 import AusschreibungsverfahrenfuerWindkraftVerdoppeln from "../laws/AusschreibungsverfahrenfuerWindkraftVerdoppeln"
 import AusschreibungsverfahrenfuerWindkraftVervierfachen from "../laws/AusschreibungsverfahrenfuerWindkraftVervierfachen"
 import AusschreibungsverfahrenfuerWindkraftVerachtfachen from "../laws/AusschreibungsverfahrenfuerWindkraftVerachtfachen"
+import { markdown } from "../lib/utils"
+//import { uba2020DeutscheTreibhausgasEmissionen } from "../citations"
 
 export default defineEvent({
   title: "Abstimmung zur Ausschreibung von Windkraftanlagen",
@@ -20,4 +22,23 @@ export default defineEvent({
   probability() {
     return Math.random()
   },
+  citations: [],
+  //citations: [uba2020DeutscheTreibhausgasEmissionen], Hier müssen Referenzen auch aufgeführt werden, mit Komma getrennt.
+  details: markdown`
+    # Bauarbeiten
+
+    Leider gibt es hier noch keine genaueren Informationen.
+
+    # Mitarbeit gewünscht!
+
+    Gerne kannst Du selber etwas beitragen. Was fehlt hier? Kennst Du Quellen, die Hintergrund liefern oder Aussagen belegen?
+
+    In unserem git repo unter [\`src/events/\`](https://github.com/neustartklima/ich-kann-klima/tree/main/src/events) findest Du die zugehörige Datei.
+  `,
+  internals: markdown`
+    # Platz für interne Kommentare
+
+    Dies wird der Spieler nie sehen. **Zitate** können so eingefügt werden: \${cite(uba2020DeutscheTreibhausgasEmissionen)}.
+    Einfach den _backslash_ entfernen, und die richtige Quelle verwenden natürlich... ;-)
+  `,
 })
