8850d52599b41d021b8a4730e359915df3c912e3
Mathias de Riese <mdrie@fastmail.com>
2021-09-17 16:24:32 +0200

    Laws for Stromspeicherung.

---
+++ src/laws/StromspeicherungErleichtern.ts
@@ -0,0 +1,43 @@
+import { defineLaw } from "../Factory"
+import { linear } from "../lawTools"
+import { Change, modify } from "../params"
+import { markdown } from "../lib/utils"
+
+export default defineLaw({
+  title: "Stromspeicherung erleichtern",
+  description:
+    "Bürokratische Hürden für den Bau von Speicheranlagen und Einspeisung von gespeichertem Strom werden abgeschafft.",
+
+  effects(game, startYear, currentYear): Change[] {
+    return [modify("electricityGridQuality").byValue(0.2)]
+  },
+
+  priority(game) {
+    const v = game.values
+    return linear(70, 30, v.electricityGridQuality)
+  },
+  citations: [],
+  details: markdown`
+
+  `,
+  internals: markdown`
+    # Happy Path 1.7
+
+    # Folgen
+
+    Diese Folgen sind völlig aus der Luft gegriffen.
+    TODO #78: Tatsächliche Folgen recherchieren, korrigieren und belegen werden.
+
+    - [x] Die Netzqualität steigt jährlich um 0.2%.
+
+    # Voraussetzungen
+
+    - Priorität > 0
+
+    # Priorität
+
+    - 0% bei einer Netzqualität von 80%. (Zu Beginn: 50%)
+    - 100% bei einer Netzqualität von 40%.
+    - linear interpoliert
+  `,
+})
