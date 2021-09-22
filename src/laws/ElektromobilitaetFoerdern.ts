1d8d95d3db0a4aad9756a923a6dd586bd8b5c7cc
Mathias de Riese <mdrie@fastmail.com>
2021-09-22 12:43:13 +0200

    Happy Path: Mobility Laws.

---
+++ src/laws/ElektromobilitaetFoerdern.ts
@@ -0,0 +1,55 @@
+import { defineLaw } from "../Factory"
+import { MrdEuro } from "../types"
+import { lawIsAccepted, linear } from "../lawTools"
+import { Change, modify } from "../params"
+import { markdown } from "../lib/utils"
+
+export default defineLaw({
+  title: "Elektromobilität Fördern",
+  description: "Höhere Kaufprämien, Steuerbefreiung, günstiges Laden für E-Autos.",
+
+  effects(game, startYear, currentYear): Change[] {
+    if (lawIsAccepted(game, "LadeinfrastrukturAusbauen")) {
+      return [
+        modify("stateDebt").byValue(5 as MrdEuro),
+        modify("carRenewablePercentage").byValue(1),
+        modify("popularity")
+          .byValue(4)
+          .if(startYear === currentYear),
+      ]
+    }
+    return []
+  },
+
+  priority(game) {
+    const mobilityPercentage = (game.values.co2emissionsMobility / game.values.co2emissions) * 100
+    return linear(0, 25, mobilityPercentage)
+  },
+  citations: [],
+  details: markdown`
+
+  `,
+  internals: markdown`
+    # Happy Path 13
+
+    # Folgen
+
+    Diese Folgen sind völlig aus der Luft gegriffen.
+    TODO #78: Tatsächliche Folgen recherchieren, korrigieren und belegen.
+
+    Nur wenn "LadeinfrastrukturAusbauen" beschlossen wurde:
+    - Der Staatshaushalt wird jährlich mit 5 Mrd € stärker belastet.
+    - Anteil erneuerbar betrieber PKW steigt um 1% pro Jahr.
+    - Popularität steigt im ersten Jah um 4%
+
+    # Vorbedingungen:
+
+    - Priorität über 0%.
+
+    # Priorität
+
+    - 0 bei 0% Anteil an den CO2 Emissionen. (Zu Beginn: knapp 25%)
+    - 100 bei 25% Anteil
+    - linear interpoliert
+  `,
+})
---
+++ src/laws/NahverkehrModernisieren.ts
@@ -0,0 +1,59 @@
+import { defineLaw } from "../Factory"
+import { MrdEuro } from "../types"
+import { linear } from "../lawTools"
+import { Change, modify } from "../params"
+import { markdown } from "../lib/utils"
+import { vdvDatenFakten } from "../citations"
+
+export default defineLaw({
+  title: "Nahverkehr Modernisieren",
+  description: "Anschaffung Moderner, bequemer, emissionsfreier Fahrzeuge für den Nahverkehr wird gefördert.",
+
+  effects(game, startYear, currentYear): Change[] {
+    // Need to use carModifier and byValue() here, to ensure it does not fall below zero:
+    const carModifier = modify("carUsage").byValue(-0.01 * game.values.publicLocalUsage)
+    const carChange = carModifier.getChange(game.values)
+
+    return [
+      modify("stateDebt").byValue(3 as MrdEuro),
+      modify("publicLocalCapacity").byPercent(1),
+      modify("publicLocalUsage").byValue(-carChange),
+      carModifier,
+      modify("popularity").byValue(3),
+    ]
+  },
+
+  priority(game) {
+    const mobilityPercentage = (game.values.co2emissionsMobility / game.values.co2emissions) * 100
+    return linear(0, 10, mobilityPercentage)
+  },
+  citations: [vdvDatenFakten],
+  details: markdown`
+
+  `,
+  internals: markdown`
+    # Happy Path 16
+
+    # Folgen
+
+    Diese Folgen sind völlig aus der Luft gegriffen.
+    TODO #78: Tatsächliche Folgen recherchieren, korrigieren und belegen.
+
+    - Der Staatshaushalt wird jährlich mit 3 Mrd € stärker belastet.
+    - Die "Kapazität" des Nahverkehrs steigt jährlich um 1%.
+    - Umstieg von individual Verkehr auf Nahverkehr ist attraktiver:
+      - Nahverkehr Nutzung steigt 1% jährlich.
+      - Auto Nutzung sinkt entsprechend.
+    - Popularität steigt jährlich um 3%.
+
+    # Vorbedingungen:
+
+    - Priorität über 0%.
+
+    # Priorität
+
+    - 0 bei 0% Anteil an den CO2 Emissionen. (Zu Beginn: knapp 25%)
+    - 100 bei 10% Anteil
+    - linear interpoliert
+  `,
+})
