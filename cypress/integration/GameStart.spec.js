20ffa9a89e546adf26a52be84758b4d3424832ce
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-10-11 05:51:01 +0200

    Improves tests

--- cypress/integration/GameStart.spec.js
+++ cypress/integration/GameStart.spec.js
@@ -1,6 +1,7 @@
 describe("GameStart", () => {
   it("should display the game page when the game is started", () => {
     cy.visit("http://localhost:3000/")
+    cy.get(".game-setup").should("be.visible")
   })
 
   it("should display the first tour dialog when the game is started", () => {
