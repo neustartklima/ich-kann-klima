30888509c812e0c89ec87381f5d5458119405f16
Mathias de Riese <mdrie@fastmail.com>
2021-11-15 13:57:42 +0100

    Fix unit test.

--- tests/PeekTools.spec.ts
+++ tests/PeekTools.spec.ts
@@ -1,11 +1,13 @@
 import should from "should"
 import { getSortedValues } from "../src/components/PeekTools"
 import { initGame } from "../src/game"
-import { modify } from "../src/params"
+import { modify, zeroParams } from "../src/params"
 
 describe("getSortedValues()", () => {
   it("should return values as specified in the options", () => {
-    const ret = getSortedValues(initGame(), [modify("stateDebt").byValue(-1)])
+    const diffs = { ...zeroParams }
+    diffs.stateDebt = -1
+    const ret = getSortedValues(initGame().values, diffs, [modify("stateDebt").byValue(-1)])
     const lastRow = ret.pop()
     should(lastRow).not.be.undefined()
     lastRow?.id.should.be.equal("stateDebt")
