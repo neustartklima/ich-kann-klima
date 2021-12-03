ce54e375f41d0b113ef0f05e1482c00a78547d7d
Mathias de Riese <mdrie@fastmail.com>
2021-12-03 21:13:33 +0100

    Remove unneeded asyncs.

--- src/store/actions.ts
+++ src/store/actions.ts
@@ -14,10 +14,10 @@ interface Router {
 
 export function ActionFactory(router: Router, repository: Repository) {
   return {
-    async startGame(context: Context) {
+    startGame(context: Context) {
       const game = newGame()
       const event = prepareNextStep(game)
-      await repository.createGame(game)
+      repository.createGame(game)
       context.commit("setGameState", { game })
       context.dispatch("applyEvent", { event })
       router.push("/games/" + game.id)
@@ -27,7 +27,7 @@ export function ActionFactory(router: Router, repository: Repository) {
       if (context.state.game?.id === payload.gameId) return
       try {
         const game = await repository.loadGame(payload.gameId)
-        await repository.saveGame(game)
+        repository.saveGame(game)
         router.push("/games/" + game.id + (game.over ? "/over" : ""))
         context.commit("setGameState", { game })
       } catch (error) {
@@ -41,45 +41,45 @@ export function ActionFactory(router: Router, repository: Repository) {
       router.push("/games/" + context.state.game?.id + "/over")
     },
 
-    async acceptLaw(context: Context, payload: { lawId: LawId }) {
+    acceptLaw(context: Context, payload: { lawId: LawId }) {
       const game = { ...(context.state.game as Game) }
       const event = acceptLaw(payload.lawId, game)
-      await repository.saveGame(game)
+      repository.saveGame(game)
       context.commit("setGameState", { game })
       context.dispatch("applyEvent", { event })
-      await repository.decisionMade(game, payload.lawId, true)
+      repository.decisionMade(game, payload.lawId, true)
     },
 
-    async rejectLaw(context: Context, payload: { lawId: LawId }) {
+    rejectLaw(context: Context, payload: { lawId: LawId }) {
       const game = { ...(context.state.game as Game) }
       game.rejectedLaws = [...game.rejectedLaws, payload.lawId]
-      await repository.decisionMade(game, payload.lawId, false)
-      await repository.saveGame(game)
+      repository.decisionMade(game, payload.lawId, false)
+      repository.saveGame(game)
       context.commit("setGameState", { game })
     },
 
-    async sitOut(context: Context) {
+    sitOut(context: Context) {
       const game = { ...(context.state.game as Game) }
-      await repository.decisionMade(game, "sitOut", true)
+      repository.decisionMade(game, "sitOut", true)
       const event = sitOut(game)
-      await repository.saveGame(game)
+      repository.saveGame(game)
       context.commit("setGameState", { game })
       context.dispatch("applyEvent", { event })
     },
 
-    async advanceYear(context: Context) {
+    advanceYear(context: Context) {
       const game = { ...(context.state.game as Game) }
       const laws = game.acceptedLaws.map(getAcceptedLaw)
       game.currentYear++
       game.values = Calculator.calculateNextYear(game, laws, game.currentYear)
-      await repository.saveGame(game)
+      repository.saveGame(game)
       context.commit("setGameState", { game })
     },
 
-    async applyEvent(context: Context, payload: { event: Event | undefined }) {
+    applyEvent(context: Context, payload: { event: Event | undefined }) {
       if (!payload.event) return
       const game = { ...(context.state.game as Game) }
-      await repository.eventOccurred(game, payload.event)
+      repository.eventOccurred(game, payload.event)
       const changes = payload.event.apply(game)
       context.dispatch("applyEffects", { changes })
     },
@@ -125,7 +125,7 @@ export function ActionFactory(router: Router, repository: Repository) {
       localStorage.setItem("tour", "completed")
     },
 
-    async error(context: Context, payload: { error: Error }) {
+    error(context: Context, payload: { error: Error }) {
       context.commit("error", payload)
     },
   }
