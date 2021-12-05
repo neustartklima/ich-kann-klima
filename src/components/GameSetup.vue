215ad7c6a409f0ea9a58d27eb8bd7c08d1efb6e2
Joachim Schirrmacher <joachimschirrmacher@gmail.com>
2021-12-05 13:16:48 +0100

    Uses actual month instead of test value

--- src/components/GameSetup.vue
+++ src/components/GameSetup.vue
@@ -93,7 +93,7 @@ export default defineComponent({
       <div id="wall-right" />
     </div>
 
-    <Calendar :year="currentYear" :month="11" />
+    <Calendar :year="currentYear" :month="currentMonth" />
     <Heater />
     <PopularityIndicator :value="popularity" />
     <TVSet :with-news="!!eventText" />
