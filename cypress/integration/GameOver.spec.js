90239184a913177f8b65c0c1b2a32c3b32be2d72
Mathias de Riese <mdrie@fastmail.com>
2022-01-19 15:21:58 +0100

    Fix game over cypress test.

--- cypress/integration/GameOver.spec.js
+++ cypress/integration/GameOver.spec.js
@@ -1,13 +1,14 @@
 describe("GameOver", () => {
   it("should  display the game over dialog when 'Sit out' is clicked many times.", () => {
-    cy.visit("http://localhost:3000/games/game-id/")
+    cy.visit("http://localhost:3000/")
     cy.contains("Tour beenden").click()
     cy.get("#calendar").contains("2021")
-    // This does not work due to https://github.com/cypress-io/cypress/issues/19674 or similar.
-    //cy.contains("Aussitzen").click()
-    //Array.from({ length: 100 }).forEach((_) => cy.contains("Aussitzen").click())
-    //cy.get("#calendar").contains("2046")
-    //cy.get("h2").contains("Das Spiel ist leider zu Ende").should("be.visible")
-    //cy.contains("Neuer Versuch").should("be.visible").click()
+    Array.from({ length: 32 }).forEach((_) => {
+      cy.contains("Aussitzen").click()
+      cy.wait(100)
+    })
+    cy.get("#calendar").contains("2029")
+    cy.get("h2").contains("Das Spiel ist leider zu Ende").should("be.visible")
+    cy.contains("Neuer Versuch").should("be.visible").click()
   })
 })
