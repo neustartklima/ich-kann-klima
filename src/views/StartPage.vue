835cc5c6a2eae040e8124c1844043df24db5e0cd
Mathias de Riese <mdrie@fastmail.com>
2022-02-15 10:28:46 +0100

    Very simple start page.

---
+++ src/views/StartPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { useStore } from "../store"
+
+const store = useStore()
+</script>
+
+<template>
+  <h1>#ich-kann-klima</h1>
+  <button @click="store.dispatch('startGame', undefined)">Start game</button>
+</template>
+
+<style lang="scss" scoped>
+h1 {
+  font-family: "Droid Sans Mono", "Meleno Monaco", Consolas, "Courrier New", monospace;
+  margin: 0 1em;
+}
+
+p {
+  padding: 0.5rem;
+}
+</style>
